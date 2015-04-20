679f615038a317a3a95859ffef01a8807db98c46
MalloryEaton <meaton@harding.edu>
2015-04-19 23:01:32 -0500

    Commit

---
+++ Mythical Escape/Assets/My Stuff/Scripts/TileScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour
+{
+    public Transform transHome;
+    private Vector3 v3HomePos;
+
+    void Start()
+    {
+        v3HomePos = transHome.position;
+    }
+
+    public bool IsHome()
+    {
+        return (transform.position - v3HomePos).magnitude < 0.01;
+    }
+}
