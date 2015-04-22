9288c87056f17e05bc509ac1ff2609f4307e6899
MalloryEaton <meaton@harding.edu>
2015-04-21 21:45:32 -0500

    Commit

---
+++ Mythical Escape/Assets/My Stuff/Scripts/TileScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour
+{
+    public Transform transWin;
+    private Vector3 v3WinPos;
+
+    void Awake()
+    {
+        v3WinPos = transWin.position;
+    }
+
+    public bool IsWinPos()
+    {
+        return (transform.position - v3WinPos).magnitude < 0.01;
+    }
+}
---
+++ Mythical Escape/Assets/My Stuff/Scripts/Tiles.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiles : MonoBehaviour 
+{
+    private Component[] tiles;
+ 
+    void Awake () 
+    {
+        tiles = GetComponentsInChildren<Tiles>();
+    }
+ 
+    void Update()
+    {
+        Debug.Log("Update");
+        if(HasWon())
+        {
+            GameObject.Find("Text").GetComponent<GUIText>().text = "You Won!";
+        }
+    }
+
+    public bool HasWon()
+    {
+        foreach(TileScript t in tiles)
+        {
+            if (!t.IsWinPos())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
